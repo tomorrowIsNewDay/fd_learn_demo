51631c9ab8297739970a5e232354334dbd8381bf
lee_ming <602387427@qq.com>
2018-06-08 15:23:38 +0800

    Update 科里化.js

--- 科里化.js
+++ 科里化.js
@@ -3,3 +3,37 @@ function currying(fn,n){
         return fn.call(this,m,n);
     }
 }
+
+// 通用实现
+function currying(fn) {
+    var slice = Array.prototype.slice,
+    __args = slice.call(arguments, 1);
+    return function () {
+        var __inargs = slice.call(arguments);
+        return fn.apply(null, __args.concat(__inargs));
+    };
+}
+
+//例子
+function square(i) {
+    return i * i;
+}
+
+function dubble(i) {
+    return i *= 2;
+}
+
+function map(handeler, list) {
+    return list.map(handeler);
+}
+
+var mapSQ = currying(map, square);
+mapSQ([1, 2, 3, 4, 5]);
+mapSQ([6, 7, 8, 9, 10]);
+mapSQ([10, 20, 30, 40, 50]);
+// ......
+
+var mapDB = currying(map, dubble);
+mapDB([1, 2, 3, 4, 5]);
+mapDB([6, 7, 8, 9, 10]);
+mapDB([10, 20, 30, 40, 50]);
