daf7a625e0da91e234b2307d3ca1c4d931ca85ae
lee_ming <602387427@qq.com>
2018-07-02 11:37:30 +0800

    Create 封装jquery插件.js

---
+++ 封装jquery插件.js
@@ -0,0 +1,18 @@
+;(function($){
+  $.fn.move = function(opts){
+    var DEFAULTES = {
+    
+    }
+    opts = $.extend(true, DEFAULTES, opts);
+    // 初始化事件
+    var init = function(){
+      
+    }
+    
+    // 初始化
+    $(window).load(function(){
+        init()
+    })
+  }
+
+}(jquery));
