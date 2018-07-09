e6d0b732ade6f4c73b893ef724c2f46ad43e986d
lee_ming <602387427@qq.com>
2018-07-09 10:14:24 +0800

    Update 科里化.js

--- 科里化.js
+++ 科里化.js
@@ -37,3 +37,40 @@ var mapDB = currying(map, dubble);
 mapDB([1, 2, 3, 4, 5]);
 mapDB([6, 7, 8, 9, 10]);
 mapDB([10, 20, 30, 40, 50]);
+
+// 第二版实现
+var sub_curry = function(fn){
+    var args = [].slice.call(arguments,1);
+    return function(){
+        return fn.apply(this,args.concat([].slice.call(arguments)));
+    }
+}
+function curry2(fn,args){
+    var length = fn.length;
+    args = args || [];
+    return function(){
+        var _args = args.slice(0),arg,i;
+        for(i=0;i<arguments.length;i++){
+            arg = arguments[i];
+            _args.push(arg);
+        }
+        if(_args.length< length){
+            return curry.call(this, fn, _args);
+        }else{
+            return fn.apply(this, _args);
+        }
+    }
+}
+// 调用、
+var fn = curry2(function(a,b,c){
+    console.log([a,b,c]);
+})
+fn('a','b','c') // ['a','b','c']
+fn('a','b')('c') // ['a','b','c']
+fn('a')('b','c') // ['a','b','c']
+
+
+
+
+
+
